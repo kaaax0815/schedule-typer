54655b2aef9febb51dc559aabe5ac6c98a853a87
kaaax0815 <999999bst@gmail.com>
2021-05-03 15:10:18 +0200

    ✨ semantic release

--- release.config.js
+++ release.config.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-module.exports = require('@kaaaxcreators/config').releaseMain;
+module.exports = require('@kaaaxcreators/config').releaseNPM;
